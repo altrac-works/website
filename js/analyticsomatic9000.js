66f6fb59f2669918b20a0ee0911ac85132ab3a7c
Hunter Fuller <hfuller@pixilic.com>
2024-02-08 17:19:52 -0600

    Add source code cookiefier script to template.

---
+++ js/analyticsomatic9000.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("lol hey");
+
+    let src = null;
+    const urlParams = new URLSearchParams(window.location.search);
+    if ( urlParams.has("fbclid") ) {
+        src = "fbclid";
+    }
+    if ( urlParams.has("utm_source") ) { src = urlParams.get("utm_source"); }
+    if ( urlParams.has("source") ) { src = urlParams.get("source"); }
+    if ( urlParams.has("src") ) { src = urlParams.get("src"); }
+
+    console.log("source code = ", src);
+    if ( src !== null ) {
+        console.log("setting src cookie");
+        document.cookie = "src=" + src;
+    }
+
+}, false);
